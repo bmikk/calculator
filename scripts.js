0757f99be4675da08ee91be1c035f4e1ccc92a3d
bmikk <54866601+bmikk@users.noreply.github.com>
2020-12-02 08:24:01 -0700

    fixed the logic! all is working

--- scripts.js
+++ scripts.js
@@ -2,36 +2,129 @@
     
 //Global variables declared:
 let lastOperatorClicked = '=';
-let num1 = 0;
-let num2 = 0;
-let sum = 0;
+let num1;
+let num2;
+let sum;
+let operationJustPerformed = false;
 
 
 let calcDisplay = document.querySelector('#calculatorWindow');
 
 //Number button listeners:
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', () => clearDisplay()); //on each of these, need to add a condition where if an operation was just performed, reset the display and then add the string
 const digit1 = document.querySelector('#digit1');
-digit1.addEventListener('click', ( () => calcDisplay.textContent += "1"));
+digit1.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "1";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "1";
+    }
+}));
 const digit2 = document.querySelector('#digit2');
-digit2.addEventListener('click', ( () => calcDisplay.textContent += "2"));
+digit2.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "2";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "2";
+    }
+}));
 const digit3 = document.querySelector('#digit3');
-digit3.addEventListener('click', ( () => calcDisplay.textContent += "3"));
+digit3.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "3";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "3";
+    }
+}));
 const digit4 = document.querySelector('#digit4');
-digit4.addEventListener('click', ( () => calcDisplay.textContent += "4"));
+digit4.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "4";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "4";
+    }
+}));
 const digit5 = document.querySelector('#digit5');
-digit5.addEventListener('click', ( () => calcDisplay.textContent += "5"));
+digit5.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "5";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "5";
+    }
+}));
 const digit6 = document.querySelector('#digit6');
-digit6.addEventListener('click', ( () => calcDisplay.textContent += "6"));
+digit6.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "6";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "6";
+    }
+}));
 const digit7 = document.querySelector('#digit7');
-digit7.addEventListener('click', ( () => calcDisplay.textContent += "7"));
+digit7.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "7";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "7";
+    }
+}));
 const digit8 = document.querySelector('#digit8');
-digit8.addEventListener('click', ( () => calcDisplay.textContent += "8"));
+digit8.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "8";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "8";
+    }
+}));
 const digit9 = document.querySelector('#digit9');
-digit9.addEventListener('click', ( () => calcDisplay.textContent += "9"));
+digit9.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "9";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "9";
+    }
+}));
 const digit0 = document.querySelector('#digit0');
-digit0.addEventListener('click', ( () => calcDisplay.textContent += "0"));
+digit0.addEventListener('click', ( () => {
+    if (operationJustPerformed) {
+        clearDisplay();
+        operationJustPerformed = false;
+        calcDisplay.textContent += "0";
+        console.log("condition successfully triggered");
+    } else {
+        calcDisplay.textContent += "0";
+    }
+}));
+
+//Operation button listeners
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => clearDisplay()); 
 
 const addButton = document.querySelector('#add');
 addButton.addEventListener('click', () => {
@@ -39,6 +132,7 @@ addButton.addEventListener('click', () => {
         lastOperatorClicked = '+';
         num1 = parseInt(calcDisplay.textContent, 10);
         clearDisplay();
+        console.log("add button - no operation triggered");
     } else {
         num2 = parseInt(calcDisplay.textContent, 10);
         operate();
@@ -47,11 +141,57 @@ addButton.addEventListener('click', () => {
     }
 });
 
+const subButton = document.querySelector('#subtract');
+subButton.addEventListener('click', () => {
+    if (lastOperatorClicked == '=') {
+        lastOperatorClicked = '-';
+        num1 = parseInt(calcDisplay.textContent, 10);
+        clearDisplay();
+        console.log("subtract button - no operation triggered");
+    } else {
+        num2 = parseInt(calcDisplay.textContent, 10);
+        operate();
+        num1 = sum;
+        lastOperatorClicked = '-';
+    }
+});
+
+const multiplyButton = document.querySelector('#multiply');
+multiplyButton.addEventListener('click', () => {
+    if (lastOperatorClicked == '=') {
+        lastOperatorClicked = '*';
+        num1 = parseInt(calcDisplay.textContent, 10);
+        clearDisplay();
+        console.log("multiply button - no operation triggered");
+    } else {
+        num2 = parseInt(calcDisplay.textContent, 10);
+        operate();
+        num1 = sum;
+        lastOperatorClicked = '*';
+    }
+});
+
+const divideButton = document.querySelector('#divide');
+divideButton.addEventListener('click', () => {
+    if (lastOperatorClicked == '=') {
+        lastOperatorClicked = '/';
+        num1 = parseInt(calcDisplay.textContent, 10);
+        clearDisplay();
+        console.log("divide button - no operation triggered");
+    } else {
+        num2 = parseInt(calcDisplay.textContent, 10);
+        operate();
+        num1 = sum;
+        lastOperatorClicked = '/';
+    }
+});
+
+
 const equalsButton = document.querySelector('#equals');
 equalsButton.addEventListener('click', () => {
     if (lastOperatorClicked !== '=') {
         num2 = parseInt(calcDisplay.textContent, 10);
-        num1 = operate();
+        operate();
         lastOperatorClicked = '=';
     } else {
         clearDisplay();
@@ -59,60 +199,36 @@ equalsButton.addEventListener('click', () => {
 });
 
 
-//let's try something else for the buttons.
-//what it comes down to, is that when an operator button is clicked, we do this:
-    //if there's no number stored, we store it
-        //then we store the operator that was clicked (in case the equals sign is clicked)
-        //then we clear the display
-    //if there is a number stored, we need to: 
-        //operate that number with whatever is on the screen using the last clicked operator, and store the new number
-        //clear the display
-
-//then, when the equals button is clicked:
-    //simply operate the stored number with the screen number, and store it
-    //display the stored number
-    //set the last operator clicked to '=', or nothing, or 0 or something.
-    //reset the number variables so that we're ready for a new calculation.
-    //THIS LEAVES US WITH A PROBLEM. BECAUSE WE USE THE SCREEN AS A VARIABLE, WE ARE NOT READY FOR ANOTHER CALCULATION
-
-//How do we solve that? 
-    //If we use two variables, we don't need to use the screen, and it's ready to display whatever we need, whenever.
-        //if we do that, we should never operate using the screen content, only the second variable.
-        //we can use num2 as a running total, which we can display as needed on the screen
-
-
-//operator button click
-    //if num1 == 0:
-        //set num1 = displayNumber
-        //lastOperatorClicked = 'clicked operator'
-        //clear display
-    //if num1 !== 0:
-        //num2 = displayNumber
-        //sum = operate() num1 and num2
-        //set display to show sum
-        //num1 = sum
-        //set lastOperatorClicked to 'clicked operator
-
-
-
-
 //Functions ready to be called:
 function add() {
-    let sum = num1 + num2;
-    logVariables();
+    sum = num1 + num2;
     calcDisplay.textContent = sum;
+    operationJustPerformed = true;
+    logVariables();
 }
 
 function subtract() {
     sum = num1 - num2;
+    calcDisplay.textContent = sum;
+    operationJustPerformed = true;
+    console.log("subtraction performed");
+    logVariables();
 }
 
 function multiply() {
     sum = num1 * num2;
+    calcDisplay.textContent = sum;
+    operationJustPerformed = true;
+    console.log("multiplication performed");
+    logVariables();
 }
 
 function divide() {
     sum = num1 / num2;
+    calcDisplay.textContent = sum;
+    operationJustPerformed = true;
+    console.log("division performed");
+    logVariables();
 }
 
 function clearDisplay() {
@@ -153,6 +269,8 @@ function logVariables() {
     console.log("sum is " + sum);
     console.log("num1 is " + num1);
     console.log("num2 is " + num2);
+    console.log("Operation just performed? " + operationJustPerformed);
+    console.log("Last Operator clicked is " + lastOperatorClicked);
     console.log("calcDisplay.textContent is " + calcDisplay.textContent);
 }
 
